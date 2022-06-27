dd93fd372aa98a7837e9d2c6cae7e084964e0d95
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-06-27 17:40:08 +0500

    Working on events for the add and remove

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
 const form = document.querySelector('.form');
 const btnSubmit = document.querySelector('.submit');
+const btnRemove = document.querySelector('.btn-remove');
+//const bookDisplaySection = document.querySelector('book-display');
 
 class Book {
-    constructor(title, author) {
+    constructor(title, author, id) {
         this.title = title;
         this.author = author;
+        this.id = id;
     }
 }
 
@@ -13,15 +16,39 @@ class Book {
 
 console.log(form);
 
+let bookid = 0;
+
 btnSubmit.addEventListener('click', ()=> {
     console.log('form submitted');
+    
     const title = document.querySelector('.form-title').value;
     const author = document.querySelector('.form-author').value;
 
     console.log(title, author);
+    bookid += 1;
     
-    const book = new Book(title, author);
+    const book = new Book(title, author, bookid);
+    
     console.log(book);
-
+    appendDiv(book);
 });
 
+document.addEventListener('click', ()=> {
+    console.log('Btn clicked');
+}, true);
+
+function appendDiv(book){
+    let titleElement = " ";
+    titleElement += `
+          <div class="book" data-id="${book.bookid}">
+            <h2 class="title">${book.title}
+                <span class="author">${book.author}</span>
+             </h2>
+            <button class="btn btn-remove">Remove</button>
+            <hr/>
+          </div>
+        `;
+
+    const sectionBookDisplay = document.querySelector('.book-display');
+    sectionBookDisplay.innerHTML += titleElement;
+}
