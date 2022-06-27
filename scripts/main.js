bc374b4df48a23ad5b86abb346b390143d6081ea
gak92 <44802922+gak92@users.noreply.github.com>
2022-06-27 19:08:05 +0500

    Remove linters error

--- scripts/main.js
+++ scripts/main.js
@@ -3,42 +3,43 @@ const btnSubmit = document.querySelector('.submit');
 const btnRemove = document.querySelector('.btn-remove');
 
 class Book {
-    constructor(title, author, id) {
-        this.title = title;
-        this.author = author;
-        this.id = id;
-    }
+  constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
 }
 
 let bookid = 0;
 
-btnSubmit.addEventListener('click', ()=> {
-    
-    const title = document.querySelector('.form-title').value;
-    const author = document.querySelector('.form-author').value;
+btnSubmit.addEventListener('click', () => {
+  const title = document.querySelector('.form-title').value;
+  const author = document.querySelector('.form-author').value;
 
-    console.log(title, author);
-    bookid += 1;
-    
-    const book = new Book(title, author, bookid);
-    
-    appendDiv(book);
+  console.log(title, author);
+  bookid += 1;
+
+  const book = new Book(title, author, bookid);
+
+  appendDiv(book);
 });
 
 const bookDisplay = document.querySelector('.book-display');
 
-bookDisplay.addEventListener('click', (e)=> {
-
-    if (e.target.tagName === 'BUTTON'){
-        const currentDiv = e.target.parentElement;
-        currentDiv.parentElement.removeChild(currentDiv);
+bookDisplay.addEventListener(
+  'click',
+  (e) => {
+    if (e.target.tagName === 'BUTTON') {
+      const currentDiv = e.target.parentElement;
+      currentDiv.parentElement.removeChild(currentDiv);
     }
-    
-}, true);
+  },
+  true
+);
 
-function appendDiv(book){
-    let titleElement = " ";
-    titleElement += `
+function appendDiv(book) {
+  let titleElement = ' ';
+  titleElement += `
           <div class="book" data-id="${book.id}">
             <h2 class="title">${book.title}
                 <span class="author">${book.author}</span>
@@ -48,6 +49,6 @@ function appendDiv(book){
           </div>
         `;
 
-    const sectionBookDisplay = document.querySelector('.book-display');
-    sectionBookDisplay.innerHTML += titleElement;
+  const sectionBookDisplay = document.querySelector('.book-display');
+  sectionBookDisplay.innerHTML += titleElement;
 }
