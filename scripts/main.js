5ca2082dc5f6c93725715f9658d86867f5354df8
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-06-29 12:41:26 +0500

    Updated js file to improve classes

--- scripts/main.js
+++ scripts/main.js
@@ -14,54 +14,56 @@ class Books {
         this.bookid = bookid;
         this.title = title;
         this.author = author;
-    }
+    }  
+}
 
-    saveData(dataObj) {
-      const dataString = JSON.stringify(dataObj);
-      localStorage.setItem('bookList', dataString);
-    }
-      
-    getData(key) {
-      return JSON.parse(localStorage.getItem(key));
-    }
+class UI {
+  saveData(dataObj) {
+    const dataString = JSON.stringify(dataObj);
+    localStorage.setItem('bookList', dataString);
+  }
     
-    getBookList() {
-      let books = ' ';
-      deserializedBookList = this.getData('bookList');
-    
-      deserializedBookList.forEach((value) => {
-        books += `
-                <div class="book" data-id="${value.bookid}">
-                  <h2 class="title">${value.title}
-                    <span class="author">${value.author}</span>
-                  </h2>
-                  <button class="btn btn-remove" type="button">Remove</button>
-                  <hr/>
-                </div>
-                  `;
-      });
-    
-      const booklist = document.querySelector('.book-display');
-      booklist.innerHTML = books;
-    }
+  getData(key) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+  
+  getBookList() {
+    let books = ' ';
+    deserializedBookList = this.getData('bookList');
+  
+    deserializedBookList.forEach((value) => {
+      books += `
+              <div class="book" data-id="${value.bookid}">
+                <h2 class="title">${value.title}
+                  <span class="author">${value.author}</span>
+                </h2>
+                <button class="btn btn-remove" type="button">Remove</button>
+                <hr/>
+              </div>
+                `;
+    });
+  
+    const booklist = document.querySelector('.book-display');
+    booklist.innerHTML = books;
+  }
 
-    addToList(bookObj) {
-      bookListObj.push(bookObj);  
-      this.saveData(bookListObj);
-      this.getBookList();
-    }
+  addToList(bookObj) {
+    bookListObj.push(bookObj);  
+    this.saveData(bookListObj);
+    this.getBookList();
+  }
 
-    removeFromList(e) {
-      const currentDiv = e.target.parentElement;
-      currentDiv.parentElement.removeChild(currentDiv);
-    
-      const bookId = parseInt(currentDiv.dataset.id, 10);
-      const bookList = this.getData('bookList');
-      const temp = bookList.filter((item) => item.bookid !== bookId);
-    
-      this.saveData(temp);
-      this.getBookList();
-    }
+  removeFromList(e) {
+    const currentDiv = e.target.parentElement;
+    currentDiv.parentElement.removeChild(currentDiv);
+  
+    const bookId = parseInt(currentDiv.dataset.id, 10);
+    const bookList = this.getData('bookList');
+    const temp = bookList.filter((item) => item.bookid !== bookId);
+  
+    this.saveData(temp);
+    this.getBookList();
+  }
 }
 
 // Submit Data Event Handler
@@ -69,8 +71,11 @@ btnSubmit.addEventListener('click', () => {
   title = document.querySelector('.form-title').value;
   author = document.querySelector('.form-author').value;
 
-  let dummy = new Books();
-  bookListObj = dummy.getData('bookList');
+  /*let dummy = new Books();
+  bookListObj = dummy.getData('bookList');*/
+
+  let getUI = new UI();
+  bookListObj = getUI.getData('bookList');
   
   if (bookListObj !== null && bookListObj.length > 0) {
     const lastObject = bookListObj[bookListObj.length - 1];
@@ -80,10 +85,11 @@ btnSubmit.addEventListener('click', () => {
     bookListObj = [];
   }
   
-  let obj1 = new Books(bookid, title, author);
-  console.log(obj1);
+  let addBook = new Books(bookid, title, author);
+  console.log(addBook);
 
-  obj1.addToList(obj1);
+  //obj1.addToList(obj1);
+  getUI.addToList(addBook);
 
   document.querySelector('.form-title').value = '';
   document.querySelector('.form-author').value = '';
@@ -96,8 +102,8 @@ bookDisplay.addEventListener(
   'click',
   (e) => {
     if (e.target.tagName === 'BUTTON') {
-      let removeBook = new Books();
-      removeBook.removeFromList(e);
+      let removeBookUI = new UI();
+      removeBookUI.removeFromList(e);
     }
   },
   true,
@@ -107,6 +113,6 @@ bookDisplay.addEventListener(
 // Load Data Initially if there is any
 const key = localStorage.getItem('bookList');
 if (key) {
-  let getbooklists = new Books();
-  getbooklists.getBookList();
+  let getbookUI = new UI();
+  getbookUI.getBookList();
 }
