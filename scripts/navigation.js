b5fb4975b79117e217bccf00d226b1c2b72ebfaa
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-06-29 21:38:26 +0500

    Fixed Linter Errors

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,13 +1,14 @@
-function hideAllSections(){
-  var sections = document.querySelectorAll("section");
-  for(let i=0; i<sections.length;i++){
-    if(!sections[i].classList.contains("hidden"))
-      sections[i].classList.add("hidden");
+function hideAllSections() {
+  var sections = document.querySelectorAll('section');
+  for (let i = 0; i < sections.length; i += 1) {
+    if (!sections[i].classList.contains('hidden')) {
+      sections[i].classList.add('hidden');
+    }
   }
 }
 
-function showSection(section){
-  let s = document.querySelector(section);
+function showSection(section) {
+  const s = document.querySelector(section);
   hideAllSections();
-  s.classList.remove("hidden");
+  s.classList.remove('hidden');
 }
